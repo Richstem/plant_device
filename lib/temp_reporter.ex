7f8713244fd4a550aa305520ae3e8b02243f678f
Rich <richstem@gmail.com>
2026-04-25 19:08:53 -0400

    formatting and removing dead code

--- lib/temp_reporter.ex
+++ lib/temp_reporter.ex
@@ -7,49 +7,33 @@ defmodule PlantDevice.TempReporter  do
   end
 
   def init(_) do
-  socket_opts = [url: "ws://192.168.1.134:4000/socket/websocket"]
-  {:ok, socket} = Socket.start_link(socket_opts)
+    socket_opts = [url: "ws://192.168.1.134:4000/socket/websocket"]
+    {:ok, socket} = Socket.start_link(socket_opts)
 
-  # Check if we can join now, otherwise retry in 5s
-  case Channel.join(socket, "device:temp") do
-    {:ok, _resp, channel} ->
-      send(self(), :report)
-      {:ok, %{channel: channel, socket: socket}}
+    # Check if we can join now, otherwise retry in 5s
+    case Channel.join(socket, "device:temp") do
+      {:ok, _resp, channel} ->
+        send(self(), :report)
+        {:ok, %{channel: channel, socket: socket}}
 
-    _error ->
-      # Server or WiFi isn't ready. Wait 5s and try again.
-      Process.send_after(self(), :retry_join, 5000)
-      {:ok, %{channel: nil, socket: socket}}
+      _error ->
+        # Server or WiFi isn't ready. Wait 5s and try again.
+        Process.send_after(self(), :retry_join, 5000)
+        {:ok, %{channel: nil, socket: socket}}
+    end
   end
-end
 
-# Add this handler to perform the retry
-def handle_info(:retry_join, state) do
-  case Channel.join(state.socket, "device:temp") do
-    {:ok, _resp, channel} ->
-      send(self(), :report) # Start the reporting loop now
-      {:noreply, %{state | channel: channel}}
-    _error ->
-      Process.send_after(self(), :retry_join, 5000)
-      {:noreply, state}
+  # Add this handler to perform the retry
+  def handle_info(:retry_join, state) do
+    case Channel.join(state.socket, "device:temp") do
+      {:ok, _resp, channel} ->
+        send(self(), :report) # Start the reporting loop now
+        {:noreply, %{state | channel: channel}}
+      _error ->
+        Process.send_after(self(), :retry_join, 5000)
+        {:noreply, state}
+    end
   end
-end
-  # def init(_) do
-  #   socket_opts = [
-  #      url: "ws://192.168.1.134:4000/socket/websocket",
-  #      reconnect: true
-  #   ]
-
-  #   #Connecting here
-  #   {:ok, socket} = Socket.start_link(socket_opts)
-
-  #   #Joining device:temp channel
-  #   {:ok, _response, channel} = Channel.join(socket, "device:temp")
-
-  #   #Begin reporting loop
-  #   send(self(), :report)
-  #   {:ok, %{channel: channel}}
-  # end
 
   def handle_info(:report, state) do
     #processing self-sent report, sending new temp
